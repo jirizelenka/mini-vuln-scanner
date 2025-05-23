f2176109b2443759d17c8e28ae7c6d38076d1c64
George Green <zelenkaj91@gmail.com>
2025-05-23 21:09:53 +0200

    Adding Shodan.io for gathering more target info via API

--- scanner.py
+++ scanner.py
@@ -1,27 +1,31 @@
-import requests
 from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
-from payloads import XSS_PAYLOAD
+import requests
+from payloads import XSS_PAYLOADS
 
 class Scanner:
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, payloads=None):
+        self.payloads = payloads or XSS_PAYLOADS
 
-    def scan(self):
-        parsed = urlparse(self.url) # parsing url
-        query = parse_qs(parsed.query) # creating a dictionary
+    def is_vulnerable(self, url):
+        parsed = urlparse(url)  # parsing url
+        query_params = parse_qs(parsed.query)
 
-        for payload in XSS_PAYLOAD:
-            print(f"[-] Testuji XSS payload: {payload}")
-
-            modified_query = {k: payload for k in query}
-            new_query = urlencode(modified_query, doseq=True) # doseq mi spravne posklada query - https://docs.python.org/3/library/urllib.parse.html
-            test_url = urlunparse(parsed._replace(query=new_query))
+        vulnerable = False
+        for param in query_params:
+            original = query_params[param][0]
+            query_params[param][0] = self.payload
+            new_query = urlencode(query_params, doseq=True) # doseq mi spravne posklada query - https://docs.python.org/3/library/urllib.parse.html
+            new_url = urlunparse(parsed._replace(query=new_query))
 
             try:
-                response = requests.get(test_url, timeout=5)
-                if payload in response.text:
-                    print(f"[!] Úspěch, tento web je zranitelný! {test_url}")
-                else:
-                    print("[ ] Zranitelnost se nepodařila nalézt.")
+                response = requests.get(new_url, timeout=5)
+                if self.payloads in response.text:
+                    print(f"[!] XSS zranitelnost nalezena na {new_url}")
+                    vulnerable = True
             except requests.RequestException as e:
-                print(f"[!] Chyba: {e}")
+                print(f"[chyba] {new_url}: {e}")
+
+            # iterace
+            query_params[param][0] = original
+
+        return vulnerable
